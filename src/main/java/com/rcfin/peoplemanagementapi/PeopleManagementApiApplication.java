1887f98520babd172a2f69dd2eafd128df123dfe
Raphael Cavalcanti <46363242+raphaelcavalcanti0@users.noreply.github.com>
2021-12-20 14:41:10 -0300

    Start project

---
+++ src/main/java/com/rcfin/peoplemanagementapi/PeopleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.rcfin.peoplemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleManagementApiApplication.class, args);
+	}
+
+}
